ba42156a56a569cd8d7a843394e2b8f180da44b5
Wen Loong Ng <wenloongng@gmail.com>
2020-01-18 14:21:05 -0800

    Initialized Empty Components

---
+++ frontend/src/component/inclass/InClass.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class InClass extends Component {
+   render() {
+      return (
+         <React.Fragment>
+
+         </React.Fragment>
+      )
+   }
+}
---
+++ frontend/src/component/signin/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SignIn extends Component {
+   render() {
+      return (
+         <React.Fragment>
+
+         </React.Fragment>
+      )
+   }
+}
