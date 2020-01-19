f5e1ce3a8b491504cf0c83086a3c50541146dc0a
jstnf <justinbfigueroa@gmail.com>
2020-01-19 00:07:24 -0800

    Finish session back-end

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const sessionMap = new Map();
+const sessionNamespaceMap = new Map();
 
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+class Session {
+    constructor() {
+        this.questionVotes = 0;
+        this.slowDownVotes = 0;
+        this.voteMap = new Map();
+        this.userMap = new Map();
+    }
+}
 
 server.listen(80);
 
@@ -13,19 +23,20 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    console.log("Someone sent a socket...");
+    console.log("Main IO received socket.");
 
-    socket.on('createsession', function (user, classroomcode, sessioncode) {
-        createNew(user, classroomcode, sessioncode);
+    socket.on('createsession', function (user, classroomCode, sessionCode) {
+        createNewSession(user, classroomCode, sessionCode);
     });
 
-    socket.on('endsession', function (user, classroomcode, sessioncode) {
+    socket.on('endsession', function (user, classroomCode, sessionCode) {
         console.log(user + " ended a classroom session!");
-        const sessionnamespace = sessionMap.get(sessioncode);
-        sessionnamespace.remove(socket);
-        sessionMap.delete(sessioncode);
+        const sessionNamespace = sessionNamespaceMap.get(sessionCode);
+        sessionNamespace.removeAllListeners();
+        sessionNamespaceMap.delete(sessionCode);
 
-        socket.emit('sessionended', {classroomcode: classroomcode});
+        socket.emit('sessionended', classroomCode);
+        sessionMap.delete(sessionCode);
     });
 
     socket.on('disconnect', function () {
@@ -33,24 +44,56 @@ io.on('connection', function (socket) {
     });
 });
 
-function createNew(user, classroomcode, sessioncode) {
-    let sessionnamespace = io.of("/session-" + sessioncode).on('connection', function (socket) {
-        console.log("Someone talked to session " + sessioncode);
+function createNewSession(user, classroomCode, sessionCode) {
+    let session = new Session(new Map());
+    sessionMap.set(sessionCode, session);
+
+    let sessionNamespace = io.of("/session-" + sessionCode).on('connection', function (socket) {
+        console.log("Someone talked to namespace session " + sessionCode + ".");
+
+        let currSession = sessionMap.get(sessionCode);
 
         socket.on('session vote', function (user, choice) {
-            console.log(user + " voted for some choice! (" + choice + ")");
+            if (currSession.voteMap.has(user)) {
+                if (currSession.voteMap.get(user) === 0) {
+                    currSession.questionVotes--;
+                } else {
+                    currSession.slowDownVotes--;
+                }
+            }
+
+            if (choice === 0) {
+                console.log(user + " voted to have questions!");
+                currSession.questionVotes++;
+            } else {
+                console.log(user + " voted for slowing down!");
+                currSession.slowDownVotes++;
+            }
+
+            currSession.voteMap.set(user, choice);
         })
-            .on('session join', function (user) {
-                console.log(user + " joined the session!");
-            })
-            .on('session leave', function (user) {
-                console.log(user + " left the session!");
-            })
-            .on('disconnect', function () {
-                console.log("Namespace level disconnect");
-            });
+        .on('session join', function (user) {
+            console.log(user + " joined the session!");
+
+            currSession.userMap.set(user, 1); // Add to list of members attended
+        })
+        .on('session leave', function (user) {
+            console.log(user + " left the session!");
+
+            currSession.userMap.set(user, 2); // Update to show disconnected
+        })
+        .on('session resetvotes', function (user) {
+            console.log(user + " reset the votes!");
+
+            currSession.voteMap.clear();
+            currSession.questionVotes = 0;
+            currSession.slowDownVotes = 0;
+        })
+        .on('disconnect', function () {
+            console.log("Namespace level disconnection.");
+        });
     });
 
-    sessionMap.set(sessioncode, sessionnamespace);
-    console.log(user + " created a new classroom session " + sessioncode + "!");
+    sessionNamespaceMap.set(sessionCode, sessionNamespace);
+    console.log(user + " created a new classroom session " + sessionCode + "!");
 }
