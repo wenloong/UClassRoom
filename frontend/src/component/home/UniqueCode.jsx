60f54accf1bd5a51f78bd2ff5275323a02ebbdec
jstnf <justinbfigueroa@gmail.com>
2020-01-18 23:14:11 -0800

    Random unique code function

---
+++ frontend/src/component/home/UniqueCode.jsx
@@ -0,0 +1,24 @@
+const getUniqueCode = () => {
+  let code = "";
+
+  for (i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * 62);
+
+    /**
+     * Mapping:
+     * 0 - 9        0 - 9
+     * 10 - 35      A - Z
+     * 36 - 61      a - z
+     */
+
+    if (index < 10) {
+      code = code + index;
+    } else if (index < 36) {
+      code = code + String.fromCharCode(index + 55);
+    } else {
+      code = code + String.fromCharCode(index + 61);
+    }
+  }
+
+  return code;
+};
